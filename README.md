99a0e7e982b61f8dcfbe04783d499cb5b3db41c9
Jorge <dev@bytelab.cl>
2020-02-25 15:00:07 -0300

    Uploaded complete main.cpp code withall function and updated the README file

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # Arduino ESP8266 01 SPIFFS
 
 SPIFFS on a esp8266 01 1M using arduino framework, PlatformIO on VS Code
+
+## Uploading to File System
+
+To upload files into the File System, it needs to be created a data (data is the default dir_path, if you need another oe, you will need to set it up at platformio.ini).
+
+Use
+
+```
+pio run -t uploadfs
+```
+
+## CRUD
+
+This code shows how to do a CRUD using SPIFFS library
+
+## Functions
+
+> **listFiles()**
+
+This function opens the directory given and display all files inside given directory
+
+### Create
+
+> **createFile()**
+
+This function creates a file from given directory
+
+### Read
+
+> **readFile()**
+
+This function creates a file from given directory
+
+### Update
+
+> **updateFile(String option)**
+
+This function updates a file from given directory and it receives a **_String_**. as a parameter.
+
+This function calls 2 diffeent function depending on the parameter given
+
+If **_rename_** is given it will call the **renameFile()** function, which will just rename the file name. If you give a different parameter (I used the word **update**) it will call the **updateContentFromFile()** function, which will remove all the content inside the file and save a new content into it
+
+### Delete
+
+> **deleteFile()**
+
+This function deletes (remove) a file from given directory
+
+#### Note
+
+This code was built for references only. If you think it can be improved in anyhow, please let me know
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,221 @@
 #include <Arduino.h>
 #include <FS.h>
 
+// List all files isnide /data directory
+
+void listFiles()
+{
+  SPIFFS.begin();
+  Serial.println();
+  Serial.println("Listing Files");
+  String str = "";
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next())
+  {
+    str += "File: ";
+    str += dir.fileName();
+    str += " - ";
+    str += dir.fileSize();
+    str += " bytes\r\n";
+  }
+  Serial.print(str);
+  Serial.println();
+  Serial.println("End Listing Files");
+}
+
+// Create => Crud
+void createFile()
+{
+  // Mount File System. true if mounted and false if not
+  if (SPIFFS.begin())
+  {
+    Serial.println("File System mounted");
+
+    // Set filename variable
+    File f = SPIFFS.open("/newf.txt", "r");
+
+    if (f)
+    {
+      Serial.println("File exist");
+    }
+    else
+    {
+      Serial.println("File does not exist");
+
+      // Creating File
+      File newF = SPIFFS.open("/newf.txt", "w+");
+
+      //Checking if file was created
+      if (!newF)
+      {
+        Serial.println("New file could not be created");
+      }
+      else
+      {
+        Serial.println("New File was created");
+        // Printing content inside the new file
+        newF.println("I am the content inside the new file");
+
+        Serial.println("New File content was added");
+
+        File newF2 = SPIFFS.open("/newf.txt", "r");
+
+        if (!newF2)
+        {
+          Serial.println("Could not read file content that was just created");
+        }
+        else
+        {
+          String newContent = newF2.readStringUntil('\n');
+          Serial.println(newContent);
+        }
+        newF2.close();
+      }
+
+      newF.close();
+    }
+
+    f.close();
+    Serial.println("File System unmounted");
+  }
+  else
+  {
+    Serial.println("File System could not be mounted");
+  }
+}
+
+// Read => cRud
+void readFile()
+{
+  // Mount File System. true if mounted and false if not
+  if (SPIFFS.begin())
+  {
+    Serial.println("File System mounted");
+
+    // Set filename variable
+    File file = SPIFFS.open("/f.txt", "r");
+
+    // Check if the file has any bytes to be read. 0 if empty
+    if (file.available())
+    {
+      // Set Content in a String
+      String content = file.readStringUntil('\n');
+      // Read content string
+      Serial.println(content);
+    }
+    else
+    {
+      Serial.println("File does not have any bytes to read");
+    }
+
+    file.close();
+    Serial.println("File System unmounted");
+  }
+  else
+  {
+    Serial.println("File System could not be mounted");
+  }
+}
+
+//-----------
+void renameFile()
+{
+  // Mount FileSystem
+  if (SPIFFS.begin())
+  {
+    Serial.println("File System succesfully mounted");
+
+    bool rf = SPIFFS.rename("/newf.txt", "/renamedf.txt");
+
+    if (!rf)
+    {
+      Serial.println("File could not be renamed");
+    }
+    else
+    {
+      Serial.println("File was renamed");
+    }
+  }
+  else
+  {
+    Serial.println("File System could not be mounted");
+  }
+}
+
+void updateContentFromFile()
+{
+  // Mount FileSystem
+  if (SPIFFS.begin())
+  {
+    Serial.println("File System succesfully mounted");
+
+    File f = SPIFFS.open("/renamedf.txt", "w+");
+
+    f.print("0123");
+
+    String content = f.readStringUntil('\n');
+
+    Serial.println(content);
+
+    f.close();
+  }
+  else
+  {
+    Serial.println("File System could not be mounted");
+  }
+}
+//-----------
+// Update => crUd
+void updateFile(String option)
+{
+  if (option == "rename")
+  {
+    renameFile();
+  }
+  else
+  {
+    updateContentFromFile();
+  }
+}
+
+// Delete => cruD
+void deleteFile()
+{
+  if (SPIFFS.begin())
+  {
+    Serial.println("File System Mounted");
+
+    if (SPIFFS.exists("/renamedf.txt"))
+    {
+      SPIFFS.remove("/renamedf.txt");
+
+      Serial.println("File Was Deleted");
+    }
+    else
+    {
+      Serial.println("File Does not Exists");
+    }
+  }
+  else
+  {
+    Serial.println("File System Not Mounted");
+  }
+}
+
 void setup()
 {
+  delay(2000);
   Serial.begin(115200);
-  delay(1000);
   Serial.println();
+
+  // createFile();
+  // readFile();
+  // updateFile("update"); // rename or update as parameter
+  deleteFile();
+  listFiles();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // nothing to do for now, this is just a simple test
 }
